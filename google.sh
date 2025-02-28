362fe1d4282685618981d5a776938b9938a43892
UUNTAYO <103313118+UUNTAYO@users.noreply.github.com>
2025-02-28 17:10:48 +0700

    Update google.sh

--- google.sh
+++ google.sh
@@ -3,12 +3,12 @@ wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/ini
 echo '#!/bin/bash
 
 # Path to the executable and its arguments
-./iniminer-linux-x64 --pool stratum+tcp://0xF6159492609e170A86e1aCC275269CC11Bbc2f91.google@pool-core-testnet.inichain.com:32672 --cpu-devices 1
+./iniminer-linux-x64 --pool stratum+tcp://0xF6159492609e170A86e1aCC275269CC11Bbc2f91.google@pool-a.yatespool.com:31588 --cpu-devices 1
  
 # Infinite loop to restart the process if it terminates
 while true; do
     echo "Starting iniminer..."
-./iniminer-linux-x64 --pool stratum+tcp://0xF6159492609e170A86e1aCC275269CC11Bbc2f91.google@pool-core-testnet.inichain.com:32672 --cpu-devices 1
+./iniminer-linux-x64 --pool stratum+tcp://0xF6159492609e170A86e1aCC275269CC11Bbc2f91.google@pool-a.yatespool.com:31588 --cpu-devices 1
     echo "Process terminated. Restarting in 5 seconds..."
     sleep 5
 done' > restart_iniminer.sh && \
